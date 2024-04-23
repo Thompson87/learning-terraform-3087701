20e0735d6161f4bd1d272d1cb0470cd96ad5118e
Thompson87 <122421174+Thompson87@users.noreply.github.com>
2024-04-22 19:11:59 -0500

    autoscaling

--- main.tf
+++ main.tf
@@ -33,17 +33,21 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+  # insert the 1 required variable here
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifer  = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  vpc_security_groups = [module.blog_sg.security_group_id]
+  
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
 module "blog_alb" {
